f957961b7d41f75bc8b32fee218eec5928243a5f
DiamondAhuja <ahujadiamond@gmail.com>
2024-04-27 23:13:13 -0400

    Refactor event listener in script.js to toggle between light and dark mode

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 // scripts.js
-document.addEventListener('DOMContentLoaded', function() {
-    // Add interactivity here if needed
+window.addEventListener('DOMContentLoaded', (event) => {
+    const switchButton = document.getElementById('mode-switch');
+    switchButton.addEventListener('click', function() {
+        if (document.body.classList.contains('light-mode')) {
+            document.body.classList.remove('light-mode');
+            document.body.classList.add('dark-mode');
+            this.textContent = 'Light Mode';
+        } else {
+            document.body.classList.remove('dark-mode');
+            document.body.classList.add('light-mode');
+            this.textContent = 'Dark Mode';
+        }
+    });
 });
