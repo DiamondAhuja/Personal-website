36ae8c760b6d3b06d927a82ef7bb6a5fee743160
DiamondAhuja <ahujadiamond@gmail.com>
2024-04-27 23:38:32 -0400

    fixed formatting

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // scripts.js
-window.addEventListener('DOMContentLoaded', (event) => {
-    const switchButton = document.getElementById('mode-switch');
-    switchButton.addEventListener('click', function() {
-        if (document.body.classList.contains('light-mode')) {
-            document.body.classList.remove('light-mode');
-            document.body.classList.add('dark-mode');
-            this.textContent = 'Light Mode';
-        } else {
-            document.body.classList.remove('dark-mode');
-            document.body.classList.add('light-mode');
-            this.textContent = 'Dark Mode';
-        }
-    });
+window.addEventListener("DOMContentLoaded", (event) => {
+  const switchButton = document.getElementById("mode-switch");
+  switchButton.addEventListener("click", function () {
+    if (document.body.classList.contains("light-mode")) {
+      document.body.classList.remove("light-mode");
+      document.body.classList.add("dark-mode");
+      this.textContent = "Light Mode";
+    } else {
+      document.body.classList.remove("dark-mode");
+      document.body.classList.add("light-mode");
+      this.textContent = "Dark Mode";
+    }
+  });
 });
